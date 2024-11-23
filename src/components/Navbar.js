ceab799c72d19f5b38bcec7f3c567720479b2601
AldySyahputra <165064490+AldySyahputra@users.noreply.github.com>
2024-11-23 20:49:56 +0700

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -37,7 +37,7 @@ function Navbar() {
           </div>
           <div className="md:hidden flex items-center">
             <button className="outline-none mobile-menu-button" onClick={toggleMobileMenu}>
-              <svg className="w-6 h-6 text-gray-500 hover:text-gray-400 text-opacity-50" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24">
+              <svg className="w-5 h-5 text-gray-500 hover:text-gray-400 text-opacity-50" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24">
                 <path d="M4 6h16M4 12h16M4 18h16"></path>
               </svg>
             </button>
