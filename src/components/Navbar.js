de3cd41334d8dad0b78b8d0ba84286526918f3fc
AldySyahputra <165064490+AldySyahputra@users.noreply.github.com>
2024-11-23 20:56:16 +0700

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -37,7 +37,7 @@ function Navbar() {
           </div>
           <div className="md:hidden flex items-center">
             <button className="outline-none mobile-menu-button" onClick={toggleMobileMenu}>
-              <svg className="w-5 h-5 text-gray-500 hover:text-gray-400 text-opacity-50" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24">
+              <svg className="w-6 h-6 text-gray-500 hover:text-gray-400 text-opacity-50" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24">
                 <path d="M4 6h16M4 12h16M4 18h16"></path>
               </svg>
             </button>
@@ -45,30 +45,30 @@ function Navbar() {
         </div>
       </div>
       {/* Menu Mobile */}
-      <div className={`md:hidden mobile-menu ${isMobileMenuOpen ? 'block' : 'hidden'}`}>
-        <ul>
-          <li className="active">
-            <Link className="block text-sm px-2 py-4 text-black bg-red-500 font-semibold" to="/">
-              BERANDA
-            </Link>
-          </li>
-          <li>
-            <Link className="block text-sm px-2 py-4 text-black hover:bg-red-500 transition duration-300 font-semibold" to="/produk">
-              PRODUK
-            </Link>
-          </li>
-          {/* <li>
-            <Link className="block text-sm px-2 py-4 hover:bg-red-500 transition duration-300" to="/contact">
-              HUBUNGI KAMI
-            </Link>
-          </li> */}
-          <li>
-            <a className="block text-sm px-2 py-4 text-white bg-green-500 font-semibold" href="https://wa.me/081287665556">
-              <i className="fab fa-whatsapp"></i> WhatsApp
-            </a>
-          </li>
-        </ul>
-      </div>
+      <div className={`md:hidden mobile-menu ${isMobileMenuOpen ? 'block' : 'hidden'} absolute top-16 left-0 w-full bg-white shadow-lg`}>
+  <ul>
+    <li className="active">
+      <Link className="block text-sm px-2 py-4 text-black bg-red-500 font-semibold" to="/">
+        BERANDA
+      </Link>
+    </li>
+    <li>
+      <Link className="block text-sm px-2 py-4 text-black hover:bg-red-500 transition duration-300 font-semibold" to="/produk">
+        PRODUK
+      </Link>
+    </li>
+    {/* <li>
+      <Link className="block text-sm px-2 py-4 hover:bg-red-500 transition duration-300" to="/contact">
+        HUBUNGI KAMI
+      </Link>
+    </li> */}
+    <li>
+      <a className="block text-sm px-2 py-4 text-white bg-green-500 font-semibold" href="https://wa.me/081287665556">
+        <i className="fab fa-whatsapp"></i> WhatsApp
+      </a>
+    </li>
+  </ul>
+</div>
     </nav>
   );
 }
